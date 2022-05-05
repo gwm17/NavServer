e2b0748e66f3776ba96551d5251978f6acefe9c6
Gordon McCann <gwm17@my.fsu.edu>
2022-05-04 22:41:45 -0400

    Fixed blocking bug. Added some ability to handle cases such as client closing, but parent server continuing.

--- NavServer.py
+++ NavServer.py
@@ -2,29 +2,45 @@ import socket
 
 listener_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 reader_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+writer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+#The actual loop when the server is fully connected
+def ServerLoop(address):
+    print("Connection accepted from: ", address)
+    try:
+        while True:
+            data = reader_socket.recv(4096)
+            if data:
+                writer_socket.sendall(data)
+    except KeyboardInterrupt:
+        print("Keyboard interrput received. Shutting down server.")
+        return False
+    except Exception:
+        return True
+
+#Startup and signal handling
 def RunServer(host, port):
 
-    reader_socket.setblocking(False)
     reader_socket.connect(("127.0.0.1","54913")) # blocks to connect to CAENDPPServer on Local host
 
     print("Connected to CAEN server on loopback interface...")
 
-    listener_socket.bind(("", "69420"))
+    listener_socket.bind(("", "52324"))
     listener_socket.listen(1) # allow one single connection
 
-    print("Establishing listener on :69420")
+    print("Establishing listener on :52324")
 
-    writer_socket, address = listener_socket.accept() #blocks
-    with writer_socket:
-        print("Connection accpeted from ", address)
-        try:
-            while True:
-                data = reader_socket.recv(4096) #does not block
-                if data:
-                    writer_socket.sendall(data) #blocks for all data transferred
-        except Exception as e:
-            print("Exception thrown: ", e)
+    while True:
+        print("Waiting for a client...")
+        writer_socket, address = listener_socket.accept() #blocks
+        with writer_socket:
+            if not ServerLoop(address):
+                return
+            elif reader_socket.fileno() == -1:
+                print("Parent CAEN server closed. Shutting down server.")
+                return
+            else:
+                print("Client connection closed.")
 
 
 if __name__ == "__main__":
